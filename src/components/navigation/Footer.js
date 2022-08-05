f12e2b20e9560b15fecd000639a3d6efb8e26fcd
tkyg <80114433+tkyg@users.noreply.github.com>
2022-08-05 11:05:56 -0400

    create footer

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
   return (
     <div>
-      
+      <div className="row">
+        <div className="col-6"></div>
+          <h5>Follow Us</h5>
+        <div className="col-6"></div>
+          <SocialIcon url="https://twitter.com/jaketrent" />
+          <SocialIcon url="https://facebook.com/jaketrent" />
+          <SocialIcon url="https://instagram.com/jaketrent" />
+      </div>
     </div>
   )
 }
