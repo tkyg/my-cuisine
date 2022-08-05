ac9a72658ad34fad781953505ba1cc1b8fc51669
tkyg <80114433+tkyg@users.noreply.github.com>
2022-08-05 09:28:03 -0400

    create NavBar

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
+
+const style = {
+  width: "60%",
+  margin: "5% 0 1%",
+  padding: "1em",
+}
 
 const NavBar = () => {
   return (
     <div>
-      
+      <NavLink 
+      activeStyle={{
+        fontWeight: "bolder",
+        color: "red"
+      }}
+        exact
+        style={style}
+        to="/"
+      >Home</NavLink>
+            <NavLink 
+      activeStyle={{
+        fontWeight: "bolder",
+        color: "red"
+      }}
+        exact
+        style={style}
+        to="/restaurants"
+      >Restaurants</NavLink>
+            <NavLink 
+      activeStyle={{
+        fontWeight: "bolder",
+        color: "red"
+      }}
+        exact
+        style={style}
+        to="/restaurants/new"
+      >Add New Restaurant</NavLink>
+            <NavLink 
+      activeStyle={{
+        fontWeight: "bolder",
+        color: "red"
+      }}
+        exact
+        style={style}
+        to="/about"
+      >About</NavLink>
     </div>
   )
 }
