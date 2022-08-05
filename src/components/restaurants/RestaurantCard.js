c4edaeb3604c73551b84654cb4b30964234c11b3
tkyg <80114433+tkyg@users.noreply.github.com>
2022-08-05 18:53:16 -0400

    render data on card

--- src/components/restaurants/RestaurantCard.js
+++ src/components/restaurants/RestaurantCard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import RestaurantsContainer from '../../containers/RestaurantsContainer'
 
-const RestaurantCard = () => {
+const RestaurantCard = ({ restaurant }) => {
   return (
     <div>
-      
+      <h3>{restaurant.name}</h3>
+      <img src={restaurant.image} alt={restaurant.name} />
+      <h5>{restaurant.cuisine}</h5>
+      <a href={restaurant.link}>{`Click here To visit ${restaurant.name}`}</a>
     </div>
   )
 }
--- src/components/restaurants/RestaurantList.js
+++ src/components/restaurants/RestaurantList.js
@@ -4,11 +4,11 @@ import RestaurantCard from './RestaurantCard'
 
 const RestaurantList = ({ restaurants }) => {
 
-  const restaurantCards = restaurants.map(restaurant => <RestaurantCard />)
+  const restaurantCards = restaurants.map(restaurant => <RestaurantCard  key={restaurant.id} restaurant={restaurant} />)
 
   return (
     <div>
-      <RestaurantCard />
+      {restaurantCards}
     </div>
   )
 }
