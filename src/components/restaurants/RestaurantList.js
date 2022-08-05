1062d9b1e2732d4b145a371f41d94c78fd869580
tkyg <80114433+tkyg@users.noreply.github.com>
2022-08-05 18:25:49 -0400

    deconstruct props

--- src/components/restaurants/RestaurantList.js
+++ src/components/restaurants/RestaurantList.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import RestaurantCard from './RestaurantCard'
 
+
+const RestaurantList = ({ restaurants }) => {
+
+  return (
+    <div>
+      <RestaurantCard />
+    </div>
+  )
+}
+
+export default RestaurantList
